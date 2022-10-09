96fb2f99b73c99fd90ecfd14c758ec79fe66f6dd
Vandinete <vandysouza2@gmail.com>
2022-10-09 14:43:52 -0300

    Working with four verbs

---
+++ 03_RestWithASPNETUdemy_CRUD/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult  Get (string firstNumber, string secondNumber)
+        {
+           
+            return BadRequest("Invalid Inoput");
+        }
+
+      
+
+      
+    }
+}
